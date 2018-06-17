6006f916b154f28db119463192a357fb1fc46b28
Evgeniy <j.borodin1@mail.ru>
2018-06-17 10:02:26 +0300

    Нажатие на маркер работает

--- app/src/main/java/com/deniskorotchenko/mapsp/MapsActivity.kt
+++ app/src/main/java/com/deniskorotchenko/mapsp/MapsActivity.kt
@@ -1,22 +1,19 @@
 package com.deniskorotchenko.mapsp
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.Button
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
-import java.util.jar.Manifest
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -32,84 +29,58 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        /*/ Here, thisActivity is the current activity  НЕ ТРОГАТЬ - НИХРЕНА НЕ РАБОТАЕТ, НО ПРИГОДИТСЯ
-        if (ContextCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                            Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS)
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            // Permission has already been granted
-        }*/
-
-        // Добавил маркер на Крестовский остров
-        val firstQuest = LatLng(59.971944, 30.241794)
-        mMap.addMarker(MarkerOptions()
-                .position(firstQuest)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
-                .title("Крестовский остров")
-                )
-
-        // Добавил маркер на стадион
-        val stadium = LatLng(59.972959, 30.220219)
-        mMap.addMarker(MarkerOptions()
-                .title("Стадион \"Санкт-Петербург\"")
-                .position(stadium)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
-                )
-
-        val center = LatLng(59.9367364, 30.3096995)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center,11.2F)) // переводит камеру в СПб при открытии и масштабирование карты при открытии её в первый раз
-
-        // Добавил маркер перед Адмиралтейством
-        mMap.addMarker(MarkerOptions()
-                .position(center)
-                .title("Адмиралтейство")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
-                )
-
-        // Добавил маркер Новая Голландия
-        val newHolland = LatLng(59.929470, 30.289756)
-        mMap.addMarker(MarkerOptions()
-                .position(newHolland)
-                .title("Новая Голландия")
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-                )
+        init() // в этой функции объявляем все маркеры и прочую дичь
 
         button1.setOnClickListener {
             val intent = Intent(this, QuestMapActivity::class.java)
             startActivity(intent)
         }
 
+        mMap.setOnInfoWindowClickListener(GoogleMap.OnInfoWindowClickListener{
+            magic(button1) }) // при нажатии на инфо окно появляется кнопка старта
+    }
 
+    fun init(){
+        // Добавил маркер на Крестовский остров
+        val firstQuest = LatLng(59.971944, 30.241794)
+        val markerKrestovskiy = mMap.addMarker(MarkerOptions()
+                .position(firstQuest)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                .title("Крестовский остров")
+        )
+
+        // Добавил маркер на стадион
+        val stadium = LatLng(59.972959, 30.220219)
+        val markerStadium =mMap.addMarker(MarkerOptions()
+                .title("Стадион \"Санкт-Петербург\"")
+                .position(stadium)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET))
+        )
+
+        val center = LatLng(59.9367364, 30.3096995)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center,11.2F)) /* переводит камеру в СПб
+        при открытии и совершает масштабирование карты */
+
+        // Добавил маркер перед Адмиралтейством
+        val markerCenter = mMap.addMarker(MarkerOptions()
+                .position(center)
+                .title("Адмиралтейство")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+        )
+
+        // Добавил маркер Новая Голландия
+        val newHolland = LatLng(59.929470, 30.289756)
+        val markerNewHolland = mMap.addMarker(MarkerOptions()
+                .position(newHolland)
+                .title("Новая Голландия")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        )
+    }
+
+    fun magic(button: Button){
+        button.visibility = View.VISIBLE
     }
 }
