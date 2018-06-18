e02a96faf61a920286bab3f4baa47f71541594f3
testererererererer <testererererererer@yandex.ru>
2018-06-18 14:48:42 +0300

    Добавлено разрешение геолокации

--- app/src/main/java/com/deniskorotchenko/mapsp/MapsActivity.kt
+++ app/src/main/java/com/deniskorotchenko/mapsp/MapsActivity.kt
@@ -1,8 +1,22 @@
 package com.deniskorotchenko.mapsp
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.app.DownloadManager
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.view.View
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,6 +30,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,6 +40,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -31,12 +48,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         init() // в этой функции объявляем все маркеры и прочую дичь
 
-        mMap.setOnInfoWindowClickListener(GoogleMap.OnInfoWindowClickListener{
+        mMap.setOnInfoWindowClickListener(GoogleMap.OnInfoWindowClickListener {
             val intent = Intent(this, QuestMapActivity::class.java)
-            startActivity(intent) }) // при нажатии на инфо окно осуществляется переход к заданию
+            startActivity(intent)
+        }) // при нажатии на инфо окно осуществляется переход к заданию
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.isMyLocationEnabled = true
+            Log.v("MyLocation", "TRUE")
+        } else {
+            Log.v("MyLocation", "FALSE")
+            // Show rationale and request permission.
+        }
     }
 
-    fun init(){
+    fun init() {
         // Добавил маркер на Крестовский остров
         val firstQuest = LatLng(59.971944, 30.241794)
         val markerKrestovskiy = mMap.addMarker(MarkerOptions()
@@ -48,7 +76,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Добавил маркер на стадион
         val stadium = LatLng(59.972959, 30.220219)
-        val markerStadium =mMap.addMarker(MarkerOptions()
+        val markerStadium = mMap.addMarker(MarkerOptions()
                 .title("Стадион \"Санкт-Петербург\"")
                 .position(stadium)
                 .snippet("Нажмите на это окно один раз для старта")
@@ -56,7 +84,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         )
 
         val center = LatLng(59.9367364, 30.3096995)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center,11.2F)) /* переводит камеру в СПб
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 11.2F)) /* переводит камеру в СПб
         при открытии и совершает масштабирование карты */
 
         // Добавил маркер перед Адмиралтейством
@@ -75,5 +103,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title("Новая Голландия")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
         )
+
+
     }
+
+
 }
