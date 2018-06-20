a1ae051a22f1f4136b831f3e2fdc0dbbd2e053e5
Evgeniy <j.borodin1@mail.ru>
2018-06-20 14:23:49 +0300

    Denis, I want children by you

--- app/src/main/java/com/deniskorotchenko/mapsp/fragmentright.kt
+++ app/src/main/java/com/deniskorotchenko/mapsp/fragmentright.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_quest_map.*
 import kotlinx.android.synthetic.main.fragment_fragmentright.*
+import kotlinx.android.synthetic.main.fragment_fragmentright.view.*
 
 
 /**
@@ -27,6 +28,7 @@ class fragmentright : Fragment() {
     private var mParam2: String? = null
 
     private var mListener: OnFragmentInteractionListener? = null
+    val singleton = Singleton.instance
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +41,22 @@ class fragmentright : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmentright, container, false)
+        var view = inflater.inflate(R.layout.fragment_fragmentright, container, false)
+        view.textView.text="Вы на нужном месте! \n Пройдено ${singleton.nowQuestion}/${QuestDatabase(this.activity).getNumberOfQuestions()}.\n Нажмите для продолжения."
+        view.next.setOnClickListener{
+            var db = QuestDatabase(this.activity).getNumberOfQuestions()
+            if (singleton.nowQuestion == db){
+                val intent = Intent(this.activity, Last::class.java)
+                startActivity(intent)
+            }else
+                singleton.nowQuestion++
+            val fTr = this.activity.fragmentManager.beginTransaction()
+            fTr.remove(this)
+            fTr.commit()
+
+        }
+
+        return view
     }
 
     // TODO: Rename method, update argument and hook method into UI event
