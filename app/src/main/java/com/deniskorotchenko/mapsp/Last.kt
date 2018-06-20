ab8b54c7f98ed74389478e03ad6a7329858aaa04
DenisKorotchenko <dzd1024ozd@gmail.com>
2018-06-20 23:26:40 +0300

    Добавлен вывод итогового времени

--- app/src/main/java/com/deniskorotchenko/mapsp/Last.kt
+++ app/src/main/java/com/deniskorotchenko/mapsp/Last.kt
@@ -7,12 +7,24 @@ import kotlinx.android.synthetic.main.activity_last.*
 
 class Last : AppCompatActivity() {
 
+    val singleton = Singleton.instance
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_last)
+        showResultTime()
         tostart.setOnClickListener {
             val intent = Intent(this, StartActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun showResultTime (){
+        val sec = (singleton.finishTime - singleton.startTime)/1000
+        val hours = sec / 3600
+        val minutes = sec % 3600 / 60
+        val seconds = sec % 60
+        val time = String.format("%d:%02d:%02d", hours, minutes, seconds)
+        resultTime.text = time
     }
 }
