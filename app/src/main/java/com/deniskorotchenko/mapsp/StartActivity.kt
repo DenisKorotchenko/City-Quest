fbc20171cc6da4fd405f85666528c061e079351d
Evgeniy <j.borodin1@mail.ru>
2018-06-22 11:55:40 +0300

    Ещё дизайн

--- app/src/main/java/com/deniskorotchenko/mapsp/StartActivity.kt
+++ app/src/main/java/com/deniskorotchenko/mapsp/StartActivity.kt
@@ -20,32 +20,4 @@ class StartActivity : AppCompatActivity() {
     }
 
 
-    /*fun initDataBase(){
-        val dbHelper = QuestDataBase(this)
-        val db = dbHelper.writableDatabase
-        try {
-            db.delete(QuestDataBase.TABLE, null, null)
-        }
-        catch (e : Exception){
-
-        }
-        db.execSQL(("create table if not exists " + QuestDataBase.TABLE + " ( " + QuestDataBase.ID
-                + " INTEGER PRIMARY KEY, " + QuestDataBase.QUESTION + " text " +");"))
-
-        val contentValues = ContentValues()
-        contentValues.clear()
-        contentValues.put(QuestDataBase.QUESTION, "Это первый вопрос")
-        db.insert(QuestDataBase.TABLE, null, contentValues)
-        contentValues.clear()
-        contentValues.put(QuestDataBase.QUESTION, "Это второй вопрос")
-        db.insert(QuestDataBase.TABLE, null, contentValues)
-        contentValues.clear()
-        contentValues.put(QuestDataBase.QUESTION, "Это третий вопрос")
-        db.insert(QuestDataBase.TABLE, null, contentValues)
-
-
-        db.close()
-        dbHelper.close()
-    }*/
-
 }
