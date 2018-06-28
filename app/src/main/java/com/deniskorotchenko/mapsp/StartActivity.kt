8e13daf95499432133889509fbf39434e9f518f6
Evgeniy <j.borodin1@mail.ru>
2018-06-28 17:00:59 +0300

    Запрос на мою геолокацию

--- app/src/main/java/com/deniskorotchenko/mapsp/StartActivity.kt
+++ app/src/main/java/com/deniskorotchenko/mapsp/StartActivity.kt
@@ -1,12 +1,20 @@
 package com.deniskorotchenko.mapsp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_start.*
 
 class StartActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSION_FINE_LOCATION = 1
 
 
 
@@ -19,7 +27,65 @@ class StartActivity : AppCompatActivity() {
             startActivity(intent)}
 
         supportActionBar!!.title = ""
+        show_permissipn()
     }
+
+
+    private fun show_permissipn(){
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)) {
+                showExplanation("Permission Needed", "Rationale", Manifest.permission.READ_PHONE_STATE, REQUEST_PERMISSION_FINE_LOCATION)
+            }
+            else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_PERMISSION_FINE_LOCATION)}
+        }
+        else {
+
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<String>,
+            grantResults: IntArray) {
+        if (requestCode == REQUEST_PERMISSION_FINE_LOCATION) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
+
+    private fun showExplanation(title: String,
+                                message: String,
+                                permission: String,
+                                permissionRequestCode: Int) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok) { dialog, id -> requestPermission(permission, permissionRequestCode) }
+        builder.create().show()
+    }
+
+    private fun requestPermission(permissionName: String, permissionRequestCode: Int) {
+        ActivityCompat.requestPermissions(this,
+                arrayOf(permissionName), permissionRequestCode)
+    }
+
+
 
 
 }
